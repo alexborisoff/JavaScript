d99abf6874aaa75e59505e260b5b495bb5084ea4
First Last <borisov.sasha1999@yandex.by>
2023-04-13 00:16:21 +0300

    Finished homework

---
+++ homework_15/index.js
@@ -0,0 +1,21 @@
+var srtValue = 'Value: ';
+var srtType = 'Type of variable: ';
+var bool = true;
+var string = 'test string';
+var number = 0.1 + 0.2;
+var empty = null;
+
+alert('Welcome H1');
+alert('I have 4 variables: \n 1. Boolean \n 2. String \n 3. Number \n 4. Null');
+
+alert(srtValue + bool);
+alert(srtType + typeof bool);
+alert(srtValue + string);
+alert(srtType + typeof string);
+alert(srtValue + number);
+alert(srtType + typeof number);
+alert(srtValue + empty);
+alert(srtType + typeof empty);
+
+var confirmAnswer = confirm('Do you like it?');
+console.log(confirmAnswer);
