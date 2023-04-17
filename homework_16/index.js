ca6aa39995b9f02ca8de7ff0f085114167bc5ccd
First Last <borisov.sasha1999@yandex.by>
2023-04-17 11:31:02 +0300

    upd: using IF, ELSE and '?' in ECMAScript 5

--- homework_16/index.js
+++ homework_16/index.js
@@ -7,21 +7,17 @@ var genre, userAge, reason, book;
 if (interestInBooks) {
     genre = prompt("What genre of books do you like?");
         
-        if (genre === detectiveGenre || genre === horrorGenre ||genre === fantasticGenre) {
+        if (genre === detectiveGenre || genre === horrorGenre || genre === fantasticGenre) {
         userAge = +prompt("Your age?");
             (userAge && userAge <= 10) ? book = "Murder on the Orient Express" :
             (userAge && userAge <= 20) ? book = "Alphabetical murders" :
             (userAge && userAge <= 30) ? book = "Picnic at Plague Fort" :
                                         book = "1984";
             alert(book);              
-        }   
-        
-        else {
+        }   else {
             alert("Sorry, we can't help you :(");
         }   
-} 
-    
-    else {
+}   else {
         reason = prompt("What is the reason you don't like books?");
         userAge = +prompt("Your age?");
             (userAge && userAge <= 10) ? book = 'Murder on the Orient Express' :
