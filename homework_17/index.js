2d633b994706cdc8fea73b4ae97691d3d0993257
First Last <borisov.sasha1999@yandex.by>
2023-04-20 12:50:30 +0300

    UPD: Cicle (FOR)

--- homework_17/index.js
+++ homework_17/index.js
@@ -1,9 +1,9 @@
 let userNumber = +prompt('Input number');
 let userCountIteration = +prompt('Input iteration count');
-let updValue;
 
 for (let i = 0; i <= userCountIteration; i++) {
-    updValue = userNumber * i;
+    if (i === 0) continue;
+    userNumber *= i;
 }
 
-(updValue % 2 === 0) ? alert(`${updValue} - even number`) : alert(`${updValue} - odd number`);
+(userNumber % 2 === 0) ? alert(`${userNumber} - even number`) : alert(`${userNumber} - odd number`);
