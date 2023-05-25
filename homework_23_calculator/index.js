4dd7da7b964ce44c41d0aef6ce19e09653f75db8
First Last <borisov.sasha1999@yandex.by>
2023-05-25 22:08:47 +0300

    UPD: calculator

--- homework_23_calculator/index.js
+++ homework_23_calculator/index.js
@@ -1,13 +1,13 @@
-let btn = document.getElementsByTagName('button');
-let input = document.getElementById('result');
+const btn = document.getElementsByTagName('button');
+const input = document.getElementById('result');
 let res = '';
 let cl = '';
 let a;
 
 for (let item of btn) {
     item.addEventListener('click', () => {
-        if (item.value === '=') {
-            res = input.value;
+        switch (item.value) {
+            case '=':  res = input.value;
             a = res.split(' ');
             switch(a[1]){
                 case '-': return input.value = +a[0] - +a[2];
@@ -15,10 +15,9 @@ for (let item of btn) {
                 case '/': return input.value = +a[0] / +a[2]; 
                 case '*': return input.value = +a[0] * +a[2];
             }
-        } else if (item.value === 'clear') {
-            
-        } else {
-            input.value += item.value;
-        }
+            break;
+            case 'clear': return input.value = '0';
+            default: return input.value += item.value;
+        } 
     })
 }
