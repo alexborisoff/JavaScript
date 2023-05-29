8192eac0971e0cc0a63a219f57e3412521416a86
First Last <borisov.sasha1999@yandex.by>
2023-05-30 00:22:57 +0300

    event target

--- homework_24/index.js
+++ homework_24/index.js
@@ -1,11 +1,25 @@
-// EASY Level 1
 const arrColors = ['#986464', '#859459','#679542', '#457170', '#695766', 
                 '#782121', '#671876', '#833030', '#931362', '#000000'];
 const main = document.getElementById('main');
-console.log(main.lastElementChild);
 
+// EASY Level 1
+/*
 main.addEventListener('click', (event) => {
-    if (event.target.tagName != 'DIV') return; 
-    main.lastElementChild.style.backgroundColor = arrColors[(event.target.textContent)-1];
-    main.firstElementChild.nextElementSibling.style.backgroundColor = arrColors[(event.target.textContent)-1];
+    if (event.target.tagName != 'DIV') return;
+    const listBlocks = main.querySelectorAll('div'); 
+    for (let item of listBlocks) {
+        item.style.backgroundColor = arrColors[(event.target.textContent)-1];
+    }
 });
+*/
+
+// MEDIUM Level 2
+main.addEventListener('click', (event) => {
+    if (event.target.tagName != 'DIV') return;
+    const listBlocks = main.querySelectorAll('div'); 
+    for (let item of listBlocks) {
+        item.style.backgroundColor = arrColors[(event.target.textContent)-1];
+        console.log(event.target.style.backgroundColor);
+        console.log(item);
+    }
+})
