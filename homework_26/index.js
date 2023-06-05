b57be3ecaab75a39ced20262f90d741d61a3d760
First Last <borisov.sasha1999@yandex.by>
2023-06-06 01:41:24 +0300

    localStorage

---
+++ homework_26/index.js
@@ -0,0 +1,31 @@
+const list = document.getElementById('list');
+const input = document.getElementById('inputFriend');
+const btnAdd = document.getElementById('btn');
+const count = document.querySelector('span');
+
+
+    if (localStorage.length !== 0) {
+        for (let i = 0; i < localStorage.length; i++) {
+        var p = document.createElement('p');
+        list.append(p);
+        p.textContent += localStorage.getItem('key' + i);
+        }
+
+        const numberOfFriends = document.querySelectorAll('p');
+        count.innerHTML = +numberOfFriends.length;
+    }
+
+    btnAdd.addEventListener('click', () => {
+    // Добавление "друга"  
+        const p = document.createElement('p');
+        if (input.value !== '') {
+        list.append(p);
+        p.innerHTML += input.value;
+        localStorage.setItem('key' + localStorage.length, p.textContent);
+        }
+        
+    // Отображение количества списка
+    const countFriends = list.querySelectorAll('p');
+    count.innerHTML = +countFriends.length;
+    input.value = '';
+    });
